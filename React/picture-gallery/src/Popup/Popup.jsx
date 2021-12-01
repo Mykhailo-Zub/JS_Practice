ab687a24a93504e5b4202e58e57615436456771c
Miha <mikhail.a.zub@gmail.com>
2021-12-01 09:41:56 +0200

    picture-gallery fix

--- React/picture-gallery/src/Popup/Popup.jsx
+++ React/picture-gallery/src/Popup/Popup.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Popup.module.css";
 
-function Popup({ fullSource, tags, popUpHandler }) {
+function Popup({ fullSource, popUpHandler }) {
+  useEffect(() => {
+    const offset = window.innerWidth;
+    const client = document.body.clientWidth;
+    const margin = offset - client;
+    document.body.style.overflow = fullSource ? "hidden" : "auto";
+    document.body.style.marginRight = fullSource ? `${margin}px` : "0";
+  }, [fullSource]);
+
   return (
     <div className={styles.wrapper} onClick={popUpHandler}>
       <div className={`${styles.popup} ${fullSource ? `${styles.popupActive}` : ""}`}>
-        <img className={styles.image} src={fullSource} alt={tags} />
+        <img className={styles.image} src={fullSource} alt="popup" />
       </div>
     </div>
   );
